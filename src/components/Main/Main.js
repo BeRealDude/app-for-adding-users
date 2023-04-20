a59cc5e87dca99ed1acfde2e3e2d87dd32c4862d
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-04-20 20:14:39 +0300

    feat: main, header

---
+++ src/components/Main/Main.js
@@ -0,0 +1,17 @@
+
+
+function Main() {
+    return(
+    <div className="content-wrap">
+        <div className="content-wrap__side-bar"></div>
+        <main className="content">
+        <div className="sub-header">
+            <h1 className="sub-header__users">Пользователи</h1>
+            <button className="sub-header__btn-add">Добавить</button>
+        </div>
+        </main>
+        </div>
+    )
+}
+
+export default Main;
