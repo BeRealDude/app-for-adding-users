524e1e2364f9631b19f3b888aa04973177f2486e
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2024-03-07 03:09:21 +0300

    test server 09

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const NotFoundError = require('../errors/page-not-found.js');
 const { getUser, createUser, updateUser, deleteUser } = require('../controllers/users');
 
 router.get('/users', getUser);
@@ -6,5 +7,8 @@ router.post('/users', createUser);
 router.patch('/users/:id', updateUser);
 router.delete('/users/:id', deleteUser);
 
+router.use((req, res, next) => {
+    next(new NotFoundError('Страница не найдена'));
+  });
 
 module.exports = router;
