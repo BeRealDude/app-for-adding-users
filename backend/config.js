c0454ebc5747b5ca76c052e1f807e2e7f0b1c57f
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2024-03-07 04:59:49 +0300

    add mongodbAtlas

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { NODE_ENV } = process.env;
 const { SECRET_SIGNING_KEY } = process.env;
 const { PORT = 3001 ||  process.env.PORT } = process.env;
-const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/appAddUsersDb' } = process.env;
+const { DB_ADDRESS = 'mongodb+srv://vercel-admin-user:pHgT6zcAyMxRcTia@cluster0.hfyirm4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' } = process.env;
 
 module.exports = {
   NODE_ENV,
